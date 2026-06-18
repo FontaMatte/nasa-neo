4893dc23dbcb0e19bb758c1ad75854677ec6b654
FontanelMatteo <matteo.fontanel@gmail.com>
2026-06-18 15:26:00 +0200

    Make CORS configurable and clean requirements for deploy

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 NASA_API_KEY = os.getenv("NASA_API_KEY")
 NASA_BASE_URL = "https://api.nasa.gov/neo/rest/v1"
 CACHE_TTL_SECONDS = 3600
